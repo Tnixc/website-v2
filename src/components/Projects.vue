d8cf7a7b23753c24c3b4d4b168238f4479b9c3b9
Tnixc <tnixxc@gmail.com>
2023-11-10 08:30:55 -0500

    Update Projects, Skills, VueCard, and HomeView components

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,7 +1,35 @@
 <template>
-  <h1># Projects</h1>
-  <h2>## The good stuff</h2>
-
+  <h1 class="text-7xl lg:text-9xl"># Projects</h1>
+  <h2 class="text-xl lg:text-5xl text-zinc-600">## The good stuff</h2>
+  <div class="mt-10 p-2 bg-zinc-200 gap-2 grid lg:grid-cols-3 auto-rows-auto">
+    <div
+      class="h-80 border-2 border-white bg-zinc-100 hover:shadow-lg transition-all duration-500 col-span-2"
+    >
+      <h1>Elemental</h1>
+    </div>
+    <div
+      class="h-80 border-2 border-white bg-zinc-100 hover:shadow-lg transition-all duration-500"
+    >
+      <h1>Maintainer at catppuccin</h1>
+    </div>
+    <div
+      class="h-80 border-2 border-white bg-zinc-100 hover:shadow-lg transition-all duration-500"
+    >
+      <h1>Dotfiles</h1>
+    </div>
+    <div class="flex flex-col gap-2 col-span-2">
+      <div
+        class="h-full border-2 border-white bg-zinc-100 hover:shadow-lg transition-all duration-500"
+      >
+        <h1>Website v2</h1>
+      </div>
+      <div
+        class="h-full border-2 border-white bg-zinc-100 hover:shadow-lg transition-all duration-500"
+      >
+        Website v1
+      </div>
+    </div>
+  </div>
 </template>
 <style scoped>
 * {
