b39cbbc7a65f41d7a6be469d4608f55e93ce50bb
Tnixc <tnixxc@gmail.com>
2023-11-13 12:29:20 -0500

    Add Vue Router and NotFound view

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-// import router from "./router";
+import router from "./router";
+
 // @ts-ignore
 import { MotionPlugin } from "@vueuse/motion";
 
@@ -9,6 +10,6 @@ import "@namchee/tetikus/dist/tetikus.css";
 
 const app = createApp(App);
 app.use(Tetikus);
-// app.use(router);
+app.use(router);
 app.use(MotionPlugin);
 app.mount("#app");
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,24 @@
-// import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
+import type { RouteRecordRaw } from "vue-router";
+import Home from "@/views/HomeView.vue";
+import NotFound from "@/views/NotFound.vue";
 
-// const router = createRouter({
-//   history: createWebHistory(import.meta.env.BASE_URL),
-//   routes: [],
-// });
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/:catchAll(.*)",
+    name: "NotFound",
+    component: NotFound,
+  },
+];
 
-// export default router;
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
