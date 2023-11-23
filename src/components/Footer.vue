a2875c62c868642181dd1987db9ef16dab19a613
Tnixc <tnixxc@gmail.com>
2023-11-23 10:05:18 -0500

    Refactor relativeDate function to calculate the number of days since last update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -95,46 +95,16 @@ export default {
       const d = date.getUTCDate().toString().padStart(2, "0");
       const m = (date.getUTCMonth() + 1).toString().padStart(2, "0"); // Months are 0-based, so we add 1.
       const y = date.getUTCFullYear();
-      //@ts-ignore
-      function relativeDate(date) {
-        //@ts-ignore
-        const diff = Math.round((new Date() - new Date(date)) / 1000);
-
-        const minute = 60;
-        const hour = minute * 60;
-        const day = hour * 24;
-        const week = day * 7;
-        const month = day * 30;
-        const year = month * 12;
-
-        if (diff < 30) {
-          return "just now";
-        } else if (diff < minute) {
-          return diff + " seconds ago";
-        } else if (diff < 2 * minute) {
-          return "a minute ago";
-        } else if (diff < hour) {
-          return Math.floor(diff / minute) + " minutes ago";
-        } else if (Math.floor(diff / hour) == 1) {
-          return "1 hour ago";
-        } else if (diff < day) {
-          return Math.floor(diff / hour) + " hours ago";
-        } else if (diff < day * 2) {
-          return "yesterday";
-        } else if (diff < week) {
-          return week + " days ago";
-        } else if (diff < month) {
-          return Math.floor(diff / week) + " weeks ago";
-        } else if (diff < year) {
-          return Math.floor(diff / month) + " months ago";
-        } else {
-          return Math.floor(diff / year) + " years ago";
-        }
+      function relativeDate(date: Date) {
+        const now = new Date();
+        const diffTime = Math.abs(now - date);
+        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+        return diffDays
       }
 
       const relative = relativeDate(date);
 
-      return `Last updated ${relative}, on ${d}/${m}/${y}`;
+      return `Last updated ${relative} days ago, on ${d}/${m}/${y}`;
     }
 
     update.call(this);
